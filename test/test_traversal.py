16105af057b00c429e04f4933c74905060a51b26
L4TTiCe <dheshan.mail@gmail.com>
2021-12-02 12:29:56 -0500

    Completed DFS implementation

--- test/test_traversal.py
+++ test/test_traversal.py
@@ -1,41 +1,51 @@
+import random
+
 import pygame
 import time
 
+from game.Actions import Actions
 from game.GUI import GUI
 from game.SnakeGame import SnakeGame
 from pathtraversal.DFS import DFS
 
 
-def process_game(game, view):
-    game.check_collisions()
+def make_move(game, view, action):
+    sleep_time = 0.2
+
     view.redraw_window(game)
-    view.event_handler()
+
+    game.move_snake(action)
+    game.check_collision()
+    time.sleep(sleep_time)
+
+    print("--------------------------------------")
+    print(game.board)
 
 
 def main():
-    game = SnakeGame(10, loop_around=False)
+    game = SnakeGame(2, loop_around=False)
     view = GUI()
     pygame.font.init()
     view.redraw_window(game)
 
-    sleep_time = 0.2
-
     print(game.board)
     print("_____________________________________")
-    traversal_agent = DFS(game.board)
-    actions = traversal_agent.find_path()
-    print(actions)
 
-    for action in actions:
+    while not game.is_game_over():
         # Reference:
         # https://stackoverflow.com/questions/20165492/pygame-window-not-responding-after-a-few-seconds
         pygame.event.get()
-        view.redraw_window(game)
-        game.move_snake(action)
-        game.check_collision()
-        print("--------------------------------------")
-        print(game.board)
-        time.sleep(sleep_time)
+
+        traversal_agent = DFS(game.board)
+        actions = traversal_agent.find_path()
+        print(actions)
+
+        if actions is None:
+            action = random.choice(list(Actions))
+            make_move(game, view, action)
+        else:
+            for action in actions:
+                make_move(game, view, action)
 
 
 # Press the green button in the gutter to run the script.
